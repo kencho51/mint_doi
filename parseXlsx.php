112fb3d565e13fdd156c5fee06e9bb9eaff9f13e
Ken <kencho.gigascience@gmail.com>
2020-12-08 11:58:15 +0800

    Create title array

--- parseXlsx.php
+++ parseXlsx.php
@@ -5,6 +5,7 @@ require 'vendor/shuchkin/simplexlsx/src/SimpleXLSX.php';
 $xlsx = SimpleXLSX::parse('report.xlsx');
 
 $apiLines = array();
+$titles = array();
 
 $publisher = "GigaScience";
 $ISSN = "2047-217X";
@@ -15,19 +16,25 @@ foreach ($xlsx->rows() as $record)
     //Extract only title, first author last name and first name
     if ($record[3] != "Article Title" && $record[4] !="Corresponding Author Last Name" && $record[5] !="Corresponding Author First Name")
     {
+        if (!in_array($record[3], $titles, true)) {
+            array_push($titles, $record[3]);
+        }
+//        print($record[3]."\n"); //Total records = 829.
         $desiredRecord = sprintf($crossRefFormat, $record[3], $record[5], $record[4], $ISSN."\n");
         $apiURL = str_replace(' ', '%20', $desiredRecord);
         $apiURL = str_replace(array("\n", "\r"), '', $apiURL);
-//        print($apiURL)."\n"; //830
         if (!in_array($apiURL, $apiLines, true)) { //Remove duplicate lines
             array_push($apiLines, $apiURL);
         }
     }
 }
 
-for ($i = 0; $i < count($apiLines); $i++)
-{
-    print $apiLines[$i]."\n"; //702
+print(count($titles));
+print(count($apiLines));
+
+//for ($i = 0; $i < count($apiLines); $i++)
+//{
+//    print $apiLines[$i]."\n"; //702
 //    $data = file_get_contents($apiLines[$i]);
 //    $apiResults = json_decode($data, true);
 //    foreach ($apiResults as $result) {
@@ -37,7 +44,7 @@ for ($i = 0; $i < count($apiLines); $i++)
 //            }
 //        }
 //    }
-}
+//}
 
 
 
