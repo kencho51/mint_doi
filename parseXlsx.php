e4a15e5369927dbc698592c70d7a8108ee4fc3ce
Ken <kencho.gigascience@gmail.com>
2020-12-08 15:24:06 +0800

    Remove author name from the api url

--- parseXlsx.php
+++ parseXlsx.php
@@ -2,25 +2,31 @@
 
 require 'vendor/shuchkin/simplexlsx/src/SimpleXLSX.php';
 
-$xlsx = SimpleXLSX::parse('report.xlsx');
+//$xlsx = SimpleXLSX::parse('report.xlsx');
+
+$f = fopen('php://stdin', 'r');
+$xlsx = new SimpleXLSX($f);
+fclose($f);
+
+
 
 $apiLines = array();
 $titles = array();
 
 $publisher = "GigaScience";
 $ISSN = "2047-217X";
-$crossRefFormat = "https://api.crossref.org/works?query.bibliographic=%s&query.author=%s %s&filter=issn:%s&rows=1";
+$crossRefFormat = "https://api.crossref.org/works?query.bibliographic=%s&filter=issn:%s&rows=1";
 
 foreach ($xlsx->rows() as $record)
 {
     //Extract only title, first author last name and first name
-    if ($record[3] != "Article Title" && $record[4] !="Corresponding Author Last Name" && $record[5] !="Corresponding Author First Name")
+    if ($record[3] != "Article Title")
     {
         if (!in_array($record[3], $titles, true)) {
             array_push($titles, $record[3]);
         }
 //        print($record[3]."\n"); //Total records = 829.
-        $desiredRecord = sprintf($crossRefFormat, $record[3], $record[5], $record[4], $ISSN."\n");
+        $desiredRecord = sprintf($crossRefFormat, $record[3], $ISSN."\n");
         $apiURL = str_replace(' ', '%20', $desiredRecord);
         $apiURL = str_replace(array("\n", "\r"), '', $apiURL);
         if (!in_array($apiURL, $apiLines, true)) { //Remove duplicate lines
@@ -29,22 +35,26 @@ foreach ($xlsx->rows() as $record)
     }
 }
 
-print(count($titles));
-print(count($apiLines));
+//print(count($titles));
+//print(count($apiLines)); //Total records = 702.
 
-//for ($i = 0; $i < count($apiLines); $i++)
-//{
-//    print $apiLines[$i]."\n"; //702
-//    $data = file_get_contents($apiLines[$i]);
-//    $apiResults = json_decode($data, true);
-//    foreach ($apiResults as $result) {
-//        if (is_array($result['items']) || is_object($result['items'])) {
-//            foreach ($result['items'] as $item) {
-//                print($item['title'][0].' '.$item['DOI']."\n");
-//            }
-//        }
-//    }
-//}
+for ($i = 0; $i < count($apiLines); $i++)
+{
+    $data = file_get_contents($apiLines[$i]);
+    $apiResults = json_decode($data, true);
+
+    if ($apiResults['status'] === 'ok') {
+        foreach ($apiResults['message']['items'] as $result) {
+            if (in_array($result['title'][0], $titles, true)){
+                print($result['title'][0].' '.$result['DOI'].' '. $apiResults['status']."\n");
+            } else {
+                print ($result['title'][0].' '."This article is not found in CrossRef!!!"."\n");
+            }
+        }
+    } else {
+        print ("CrossRef return status is fail!");
+    }
+}
 
 
 
--- test/parseXlsxTest.php
+++ test/parseXlsxTest.php
@@ -12,11 +12,11 @@ $title = Array (
 );
 
 $testArr = Array (
-    0 => "https://api.crossref.org/works?query.bibliographic=Mitochondrial%20Metagenomics:%20Letting%20the%20Genes%20out%20of%20the%20Bottle&query.author=Alfried%20Vogler&filter=issn:2047-217X&rows=1",
-    1 => "https://api.crossref.org/works?query.bibliographic=Bioboxes%20-%20standardised%20containers%20for%20interchangeable%20bioinformatics%20software&query.author=Michael%20Barton&filter=issn:2047-217X&rows=1",
-    2 => "https://api.crossref.org/works?query.bibliographic=Clusterflock:%20A%20Flocking%20Algorithm%20for%20Isolating%20Congruent%20Phylogenomic%20Datasets&query.author=Paul%20Planet&filter=issn:2047-217X&rows=1",
-    3 => "https://api.crossref.org/works?query.bibliographic=Fractal%20Self-similarity,%20Scale%20Invariance%20and%20Stationary%20waves%20Codes%20Architecture%20Human%20Chromosomes%20DNA%20sequences&query.author=Jean%20Perez&filter=issn:2047-217X&rows=1",
-    4 => "https://api.crossref.org/works?query.bibliographic=Mitochondrial%20Metagenomics:%20Letting%20the%20Genes%20out%20of%20the%20Bottle&query.author=Alfried%20Vogler&filter=issn:2047-217X&rows=1",
+    0 => "https://api.crossref.org/works?query.bibliographic=Mitochondrial%20Metagenomics:%20Letting%20the%20Genes%20out%20of%20the%20Bottle&filter=issn:2047-217X&rows=1",
+    1 => "https://api.crossref.org/works?query.bibliographic=Bioboxes%20-%20standardised%20containers%20for%20interchangeable%20bioinformatics%20software&filter=issn:2047-217X&rows=1",
+    2 => "https://api.crossref.org/works?query.bibliographic=Clusterflock:%20A%20Flocking%20Algorithm%20for%20Isolating%20Congruent%20Phylogenomic%20Datasets&filter=issn:2047-217X&rows=1",
+    3 => "https://api.crossref.org/works?query.bibliographic=Fractal%20Self-similarity,%20Scale%20Invariance%20and%20Stationary%20waves%20Codes%20Architecture%20Human%20Chromosomes%20DNA%20sequences&filter=issn:2047-217X&rows=1",
+    4 => "https://api.crossref.org/works?query.bibliographic=Mitochondrial%20Metagenomics:%20Letting%20the%20Genes%20out%20of%20the%20Bottle&filter=issn:2047-217X&rows=1",
 );
 
 
