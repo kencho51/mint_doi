027583758caf597f5ebefab6fa5490fa4c2cc32a
Ken <kencho.gigascience@gmail.com>
2021-01-22 18:12:34 +0800

    Testing the title return results

--- parseXlsx.php
+++ parseXlsx.php
@@ -17,6 +17,8 @@ foreach ($xlsx->rows() as $record)
     //Extract only title
     if ($record[3] != "Article Title")
     {
+        $record[3] = str_replace(array("\n", "\r"), '', $record[3]);
+        $record[3] = strtolower($record[3]);
         if (!in_array($record[3], $titles, true)) {
             array_push($titles, $record[3]);
         }
@@ -30,45 +32,49 @@ foreach ($xlsx->rows() as $record)
     }
 }
 
-//print(count($titles));
+//print_r($titles);
 //print(count($apiLines)); //Total records = 702.
+//var_dump($apiLines);
 
-$doiUrl = "https://doi.org/";
 for ($i = 0; $i < count($apiLines); $i++)
 {
     $data = file_get_contents($apiLines[$i]);
     $apiResults = json_decode($data, true);
 
-    $fp = fopen($fileToMint.'-minted.csv', 'a');
+//    $fp = fopen($fileToMint.'-'.date('Y-m-d').'.'.'minted.csv', 'a');
     if ($apiResults['status'] === 'ok') {
         foreach ($apiResults['message']['items'] as $result) {
-            if (in_array($result['title'][0], $titles, true)){
-//                print($result['title'][0].' '.$result['DOI'].' '. $apiResults['status']."\n");
-                $fields = array(
-                    'Status' => $apiResults['status'],
-                    'DOI' => $doiUrl.$result['DOI'],
-                    'Title' => $result['title'][0]
-                );
-                fputcsv($fp, $fields);
-            } else {
-//                print ($titles[$i].' '."This article is not found in CrossRef!!!"."\n");
-                $fields = array(
-                    'Status' => $apiResults['status'],
-                    'DOI' => 'The doi is not found in CrossRef!',
-                    'Title' => $result['title'][0]
-                );
-                fputcsv($fp, $fields);
-            }
+            print($result['title'][0])."\n";
+//            if (in_array(strtolower($result['title'][0]), $titles)){
+////                print($result['title'][0])."\n";
+////                print($result['title'][0].' '.$result['DOI'].' '. $apiResults['status']."\n");
+////                $fields = array(
+////                    'CrossRef API Status' => $apiResults['status'],
+////                    'URL' => $result['URL'],
+////                    'Title' => $result['title'][0]
+////                );
+////                fputcsv($fp, $fields);
+//            } else {
+////                print($result['title'][0])."\n";
+////                print($titles[$i]);
+////                print ($titles[$i].' '."This article is not found in CrossRef!!!"."\n");
+////                $fields = array(
+////                    'CrossRef API Status' => $apiResults['status'],
+////                    'URL' => 'The doi is not found in CrossRef!',
+////                    'Title' => $result['title'][0]
+////                );
+////                fputcsv($fp, $fields);
+//            }
         }
     } else {
 //        print ("CrossRef return status is fail!");
         $fields = array(
-            'Status' => 'CrossRef return status is not OK!',
-            'DOI' => 'NA',
+            'CrossRef API Status' => 'CrossRef API return status is not OK!',
+            'URL' => 'NA',
             'Title' => $apiResults['message']['items']['title'][0]
         );
     }
-    fclose($fp);
+//    fclose($fp);
 }
 
 
