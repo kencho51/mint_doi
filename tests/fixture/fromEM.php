b918b4f6dca8a8670e702288e959909742cbca36
Ken <kencho.gigascience@gmail.com>
2021-01-22 13:44:33 +0800

    Create fixture file

---
+++ tests/fixture/fromEM.php
@@ -0,0 +1,24 @@
+<?php
+
+return array(
+    array(
+        0 => "Manuscript Number",
+        1 => "Initial Date Submitted",
+        2 => "Editorial Status",
+        3 => "Article Title",
+        4 => "Corresponding Author Last Name",
+        5 => "Corresponding Author First Name",
+        6 => "E-mail Address",
+        7 => "First Author Last Name",
+        8 => "First Author First Name",
+        9 => "Manuscript Notes",
+        10 => "Final Disposition Term",
+        11 => "Publication Date",
+        12 => "All Authors",
+        13 => "Corresponding Author ORCID",
+        14 => "Funder Name",
+        15 => "Grant Number",
+        16 => "Grant Recipient",
+        17 => "Abstract",
+    ),
+);
