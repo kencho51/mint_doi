df98358d87a051d42f1fbad33d7b5999c7e268e6
Ken <kencho.gigascience@gmail.com>
2021-02-03 13:42:05 +0800

    Create files for MinDOI

--- src/MintDOI.php
+++ src/MintDOI.php
@@ -4,50 +4,12 @@ require './vendor/shuchkin/simplexlsx/src/SimpleXLSX.php';
 
 class MintDOI
 {
-    /**
-     * Store formatted url to pass in CrossRef.
-     * @var array
-     */
-    public $apiLines = array();
 
-    /**
-     * Store paper title from EM spreadsheet.
-     * @var array
-     */
-    public $titles = array();
+    public $title;
 
-    /**
-     * GigaScience as the publisher.
-     * @var string
-     */
-    protected $publisher = "GigaScience";
-
-    /**
-     * The default ISSN number for GigaScience.
-     * @var string
-     */
-    protected $ISSN = "2047-217X";
-
-    /**
-     * CrossRef API template
-     * @var string
-     */
-    public $crossRefFormat = "https://api.crossref.org/works?query.bibliographic=%s&filter=issn:%s&rows=1";
-
-    /**
-     * @param $fileName
-     * @return mixed The title of each paper.
-     */
-    public function getTitle($fileName)
+    public function getEMTitle()
     {
-        $xlsx = SimpleXLSX::parse($fileName);
-
-        foreach ($xlsx->rows() as $record)
-        {
-            if ($record[3] != "Article Title")
-            {
-                return $record[3];
-            }
-        }
+        return "";
     }
 }
+
--- tests/MintDOITest.php
+++ tests/MintDOITest.php
@@ -4,11 +4,47 @@ use PHPUnit\Framework\TestCase;
 
 class MintDOITest extends TestCase
 {
-    public function testPHPUnitIsWorking()
-    {
-        $mintDOI = new MintDOI;
+    protected $mintDOI;
 
-        $this->assertEquals(4, 2 + 2);
+    public function setUp(): void
+    {
+        $this->mintDOI = new MintDOI;
     }
 
+    public function testTitleIsEmptyByDefault()
+    {
+        $this->assertEmpty($this->mintDOI->title);
+    }
+
+    public function testGetEMTitleWithNoTitle()
+    {
+        $this->assertSame($this->mintDOI->getEMTitle(), "");
+    }
+
+    public function testGetEMTitleFromArray()
+    {
+        $EM = array(
+            0 => "Manuscript Number",
+            1 => "Initial Date Submitted",
+            2 => "Editorial Status",
+            3 => "Article Title",
+            4 => "Corresponding Author Last Name",
+            5 => "Corresponding Author First Name",
+            6 => "E-mail Address",
+            7 => "First Author Last Name",
+            8 => "First Author First Name",
+            9 => "Manuscript Notes",
+            10 => "Final Disposition Term",
+            11 => "Publication Date",
+            12 => "All Authors",
+            13 => "Corresponding Author ORCID",
+            14 => "Funder Name",
+            15 => "Grant Number",
+            16 => "Grant Recipient",
+            17 => "Abstract",
+        );
+
+
+
+    }
 }
