af62e59fc11b5147f0fce0704d748c908db66193
Ken <kencho.gigascience@gmail.com>
2021-04-29 18:22:22 +0800

    Try to escape comma

--- taverna/getFinalReport.java
+++ taverna/getFinalReport.java
@@ -2,7 +2,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
-FileWriter csvWriter = new FileWriter("/Users/kencho/Desktop/test.csv");
+csvWriter = new FileWriter("/Users/kencho/Desktop/test.csv");
 for (int i = 0; i < title.size(); i++) {
 	csvWriter.append(title.get(i));
 	csvWriter.append(",");
@@ -10,7 +10,7 @@ for (int i = 0; i < title.size(); i++) {
 	csvWriter.append(",");
 	csvWriter.append(doi.get(i));
 	csvWriter.append(",");
-	csvWriter.append(content.get(i));
+	csvWriter.append(content.get(i).replaceAll("\\n","").replaceAll(",","\\\,"));
 	csvWriter.append(",");
 	csvWriter.append("\n");
 }
